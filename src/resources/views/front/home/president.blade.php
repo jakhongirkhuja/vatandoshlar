f8e3e950c19471dd0bf847fbeb50bff1ef1c7cfe
azizovmuhammadali <alico.777777@gmail.com>
2025-12-28 01:03:34 +0500

    changed dynamic parts

--- src/resources/views/front/home/president.blade.php
+++ src/resources/views/front/home/president.blade.php
@@ -1,22 +1,29 @@
 <section class="president">
     <div class="container">
+        @php
+            $presidentSections = menuSections(4, 1);
+            $item = $presidentSections->first(); 
+        @endphp
         <div class="section-top">
-            <p>Vatandoshlar jamoat fondi </p>
+            <p> “{{ sectionValue($item, 'title') }}” </p>
         </div>
         <div class="president__wrapper">
-            <div class="president__right">
-                <p class="president__right--text">“Xorijda istiqomat qilayotgan yurtdoshlarni birlashtirish
-                    maqsadida
-                    'Vatandoshlar' jamoat fondi
-                    tashkil etildi... Bu fonddan asosiy maqsad - tarixiy Vatanimiz atrofida ularni yanada
-                    jipslashtirish, ularning qalbi va ongida yurt bilan faxrlanish tuyg'usini yuksaltirish,
-                    milliy
-                    oʻzlikni saqlab qolish”</p>
-                <p class="president__right--name">— Shavkat Miromonovich Mirziyoyev</p>
-                <span class="president__right--position">O’zbekiston Respublikasi Prezidenti</span>
-            </div>
-            <div class="president__left">
-                <img src="{{ asset('front') }}/images/president-main.png" width="520" alt="">
-            </div>
+            @if($item)
+                <div class="president__right">
+                    <p class="president__right--text">
+                        “{{ sectionValue($item, 'content') }}”
+                    </p>
+                    <p class="president__right--name">
+                        — {{ sectionValue($item, 'description') }}
+                    </p>
+                    <span class="president__right--position">
+                        {{ sectionValue($item, 'content1') }}
+                    </span>
+                </div>
+                <div class="president__left">
+                    <img src="{{ sectionImages($item, true) }}" width="520" alt="{{ sectionValue($item, 'title') }}">
+                </div>
+            @endif
         </div>
+    </div>
 </section>
--- src/resources/views/front/home/stats.blade.php
+++ src/resources/views/front/home/stats.blade.php
@@ -1,65 +1,23 @@
 <section class="stats">
     <div class="container">
         <div class="section-top">
-            <p>Statistika</p>
+            <p>{{ 'Statistika' }}</p>
         </div>
 
         <div class="stats__grid">
-            <div class="stats__grid--item">
-                <div class="stats__grid--item-img">
-                    <img src="{{ asset('front') }}/images/stats-img-1.svg" alt="">
+            @foreach(menuSections(3, 4, true) as $stat) 
+                <div class="stats__grid--item">
+                    <div class="stats__grid--item-img">
+                        <img src="{{ sectionValue($stat, 'image') ? Storage::url(sectionValue($stat, 'image')) : asset('front/images/stats-img-1.svg') }}"
+                            alt="{{ sectionValue($stat, 'title') }}">
+                    </div>
+                    <h3 class="stats__grid--item-title" data-count="{{ sectionValue($stat, 'number', 0) }}">
+                        0+
+                    </h3>
+                    <p class="stats__grid--item-subtitle">{{ sectionValue($stat, 'title', 'Qatnashgan vatandoshlar soni') }}
+                    </p>
                 </div>
-                <h3 class="stats__grid--item-title" data-count="25565">
-                    0+
-                </h3>
-                <p class="stats__grid--item-subtitle">Qatnashgan vatandoshlar soni</p>
-            </div>
-            <div class="stats__grid--item">
-                <div class="stats__grid--item-img">
-                    <img src="{{ asset('front') }}/images/stats-img-1.svg" alt="">
-                </div>
-                <h3 class="stats__grid--item-title" data-count="120">
-                    0+
-                </h3>
-                <p class="stats__grid--item-subtitle">Qatnashgan vatandoshlar soni</p>
-            </div>
-            <div class="stats__grid--item">
-                <div class="stats__grid--item-img">
-                    <img src="{{ asset('front') }}/images/stats-img-1.svg" alt="">
-                </div>
-                <h3 class="stats__grid--item-title" data-count="124683">
-                    0+
-                </h3>
-                <p class="stats__grid--item-subtitle">Qatnashgan vatandoshlar soni</p>
-            </div>
-            <div class="stats__grid--item">
-                <div class="stats__grid--item-img">
-                    <img src="{{ asset('front') }}/images/stats-img-1.svg" alt="">
-                </div>
-                <h3 class="stats__grid--item-title" data-count="8565">
-                    0+
-                </h3>
-                <p class="stats__grid--item-subtitle">Qatnashgan vatandoshlar soni</p>
-            </div>
-            <div class="stats__grid--item">
-                <div class="stats__grid--item-img">
-                    <img src="{{ asset('front') }}/images/stats-img-1.svg" alt="">
-                </div>
-                <h3 class="stats__grid--item-title" data-count="32351">
-                    0+
-                </h3>
-                <p class="stats__grid--item-subtitle">Qatnashgan vatandoshlar soni</p>
-            </div>
-            <div class="stats__grid--item">
-                <div class="stats__grid--item-img">
-                    <img src="{{ asset('front') }}/images/stats-img-1.svg" alt="">
-                </div>
-                <h3 class="stats__grid--item-title" data-count="512">
-                    0+
-                </h3>
-                <p class="stats__grid--item-subtitle">Qatnashgan vatandoshlar soni</p>
-            </div>
-
+            @endforeach
         </div>
     </div>
 </section>
